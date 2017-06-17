501b59cb581bfacdd918f6eb2f14c89d3c47ab1d
Vladislav <vladislav.kovalik@gmail.com>
2017-06-17 13:50:58 +0300

    add repository

---
+++ src/CorporateMessenger/Data/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorporateMessenger.Data.Interfaces
+{
+    public interface IRepository<T> where T : 
+    {
+        Task<T> FindAsync(int id);
+        void Add(T entyty);
+        void Remove(IEnumerable<T> entities);
+    }
+}
---
+++ src/CorporateMessenger/Data/Repository/Repository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CorporateMessenger.Data.Interfaces;
+
+namespace CorporateMessenger.Data.Repository
+{
+    internal class Repository<TEntyty> : IRep
+    {
+    }
+}
